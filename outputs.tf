112fc7e1ed59973e0669b1fd30d684c3c4719328
FabiusE <fabius.engel@msg.group>
2020-03-09 08:00:00 +0100

    More Output

--- outputs.tf
+++ outputs.tf
@@ -43,3 +43,7 @@ output "node_resource_group" {
 output "cluster_name" {
   value = "${azurerm_kubernetes_cluster.aks_cluster.name}"
 }
+
+output "resource_group_name" {
+  value = "${azurerm_resource_group.resource_group.name}"
+}
