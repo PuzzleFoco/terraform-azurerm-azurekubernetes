caa05c31f39dba8b24e8f77a8c7834c37370b4d1
FabiusE <fabius.engel@msg.group>
2020-06-04 20:19:16 +0200

    Added option to install kubernetes dashboard

--- main.tf
+++ main.tf
@@ -9,6 +9,12 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
     resource_group_name = azurerm_resource_group.resource_group.name
     dns_prefix          = var.dns_prefix
 
+    addon_profile {
+        kube_dashboard {
+            enabled = var.kube_dashboard
+    }
+  }
+
     default_node_pool {
         name        = "default"
         node_count  = var.node_count
