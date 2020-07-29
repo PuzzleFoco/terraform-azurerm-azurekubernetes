c830c26d9a900ac21640564a260a4182ad911f8b
Fabius Engel <Fabius.Engel@msg.group>
2020-07-29 21:47:32 +0200

    remove comment

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,3 @@
-/*
- * Created Date: 14.01.2020
- * Author: Fabius Engel (fabius.engel@msg.group)
- * -----
- * Last Modified: 14.01.2020 15:41:46
- * Modified By: Fabius Engel (fabius.engel@msg.group)
- * -----
- * Copyright (c) 2020 msg nexinsure ag
- */
-
 output "client_key" {
   value = "${azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_key}"
 }
